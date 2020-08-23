37a63d872fc317a12a325b17f287432ec78d69bb
LizethRubio <lizeth.rubio.zuleta@gmail.com>
2020-08-23 16:41:09 -0500

    exercise 13

---
+++ src/com/company/Exercise13.java
@@ -0,0 +1,8 @@
+package com.company;
+//13. Realice un algoritmo que lea un vector de cien elementos y que calcule su magnitud y
+//        represéntelo mediante diagrama de flujo y pseudocódigo.
+public class Exercise13 {
+    public static void main(String[] args) {
+        System.out.println("Este ejercicio no se realizó");
+    }
+}
